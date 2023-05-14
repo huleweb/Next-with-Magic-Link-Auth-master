fffc666de9a6a6414c6408fdc7f38c8371f5d97c
hule <huleweb@gmail.com>
2023-05-14 03:07:48 -0300

    mmm

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import $metadata from '../pages/_app/login'
+
 
 export default function Home() {
   return (
